b7ff7ff53983599045889cb5712868e29c21805a
Ryan A. Ripley <ryanaripley@gmail.com>
2019-02-21 15:18:12 -0600

    Add copyright to footer

--- footer.php
+++ footer.php
@@ -15,17 +15,10 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flyspace' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'flyspace' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'flyspace' ), 'flyspace', '<a href="http://fourteensquare.com">Fourteen Square</a>' );
-				?>
+			<div class="site-copyright">
+				&copy;<?php echo date("Y") . ' '; ?>
+				<?php bloginfo( 'name' ); ?>
+			</div><!-- .site-copyright -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
